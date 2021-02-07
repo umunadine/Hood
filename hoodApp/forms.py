ba906b616e33f68bfbaf55132c8a4f0c13553cf7
umunadine <umutesinad3@gmail.com>
2021-02-08 00:22:05 +0200

    business form

--- hoodApp/forms.py
+++ hoodApp/forms.py
@@ -9,3 +9,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class NewBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['neighborhood']
